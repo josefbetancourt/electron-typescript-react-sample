a2c6c4e984b7bded95b13d44a6c2228e5163e064
jxbetancourt <josef.betancourt@gmail.com>
2019-09-21 07:57:38 -0400

    can see the startup screen!

--- .webpack.config.js
+++ .webpack.config.js
@@ -1,5 +1,15 @@
+var path = require('path');
+
 // define child rescript
 module.exports = (config) => {
   config.target = 'electron-renderer';
+  config.plugins: [
+    new webpack.DefinePlugin({ "global.GENTLY": false })
+  ];
+  config.node: {
+    __dirname: true,
+  };
+  
   return config;
 };
+
